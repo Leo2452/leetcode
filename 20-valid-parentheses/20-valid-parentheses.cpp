72954a2a290fc2ca54643446d2b6a46487c0f5b4
Leo Lopez <35853927+Leo2452@users.noreply.github.com>
2022-05-15 00:34:11 -0700

    Time: 3 ms (38.77%), Space: 6.4 MB (51.26%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    bool isOpen(char c) {
+        return c == '(' || c == '[' || c == '{';
+    }
+    
+    bool isValid(char c1, char c2) {
+        return (c1 == '(' && c2 == ')') ||
+               (c1 == '[' && c2 == ']') ||
+               (c1 == '{' && c2 == '}');
+    }
+    
+    bool isValid(string str) {
+        stack<char> s;
+        for (int i = 0; i < str.size(); i++) {
+            if (isOpen(str.at(i))) {
+                s.push(str.at(i));
+            } else {
+                if (s.empty() || !isValid(s.top(), str.at(i))) {
+                    return false;
+                }
+                s.pop();
+            }
+        }
+        return s.empty();
+    }
+};
