0df5455a078c97762edf871c1ad998afb0c758c0
Leo Lopez <35853927+Leo2452@users.noreply.github.com>
2022-05-15 02:15:23 -0700

    Time: 356 ms (23.43%), Space: 7.2 MB (18.86%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,32 @@
+class Foo {
+public:
+    bool done_1_{false};
+    bool done_2_{false};
+    mutex m_;
+    condition_variable cv_;
+    
+    Foo() {}
+
+    void first(function<void()> printFirst) {
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        done_1_ = true;
+        cv_.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+        unique_lock<mutex> lock(m_);
+        cv_.wait(lock, [this] { return done_1_; });
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        done_2_ = true;
+        cv_.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        unique_lock<mutex> lock(m_);
+        cv_.wait(lock, [this] { return done_2_; });
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
