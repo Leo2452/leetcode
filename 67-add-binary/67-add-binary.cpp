6b457335638978b1447685ac54de19cc106ffc58
Leo Lopez <35853927+Leo2452@users.noreply.github.com>
2022-05-16 22:35:56 -0700

    Time: 6 ms (32.19%), Space: 7 MB (11.36%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -4,16 +4,16 @@ public:
         string result = "";
         int a_right = a.size() - 1;
         int b_right = b.size() - 1;
-        int carry = 0;
+        bool carry_over = false;
         
         while (a_right >= 0 || b_right >= 0) {
-            int sum = carry;
+            int sum = carry_over;
             if(a_right >= 0) sum += a[a_right--] - '0';
             if(b_right >= 0) sum += b[b_right--] - '0';
-            carry = sum > 1 ? 1 : 0;
+            carry_over = (sum > 1);
             result = to_string(sum % 2) + result;
         }
-        if (carry) result = to_string(1) + result;
+        if (carry_over) result = to_string(1) + result;
         return result;
     }
 };
